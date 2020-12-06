132571e6ffcba03bb92a178fcccccbc8c56e8101
Jhonny <jh.hueller@gmail.com>
2020-12-06 11:41:17 +0100

    Created a pixel art theme and started to improve visuals

--- Scenes/SudokuSolverNode.cs
+++ Scenes/SudokuSolverNode.cs
@@ -55,7 +55,7 @@ public class SudokuSolverNode : Node
 		9, 1, 2, 3, 4, 5, 6, 7, 8,
 		});
 		*/
-		/*
+		
 		List<int>test= new List<int>(new int[] {
 		1, 2, 0, 0, 5, 6, 0, 8, 9,
 		4, 5, 0, 7, 0, 9, 1, 0, 3,
@@ -67,7 +67,7 @@ public class SudokuSolverNode : Node
 		6, 7, 0, 0, 1, 2, 3, 0, 5,
 		0, 1, 2, 3, 4, 5, 6, 7, 8,
 		});
-		*/
+		
 		/*
 		List<int>test= new List<int>(new int[] {
 		8, 2, 0, 0, 1, 0, 0, 0, 3,
@@ -81,7 +81,7 @@ public class SudokuSolverNode : Node
 		6, 0, 0, 0, 2, 0, 0, 5, 9,
 		});
 		*/
-		
+		/*
 		List<int>test= new List<int>(new int[] {
 		0, 2, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 6, 0, 0, 0, 0, 3,
@@ -93,7 +93,7 @@ public class SudokuSolverNode : Node
 		5, 0, 0, 0, 0, 9, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 4, 0,
 		});
-		
+		*/
 
 		int i = 0;
 		foreach(Godot.Collections.Array row in grid)
